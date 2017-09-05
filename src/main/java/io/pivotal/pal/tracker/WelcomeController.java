f4bcc0658ef2538ec49e8524a200f72f82586529
tobato <tobato@msn.com>
2017-09-05 17:02:39 +0800

    Setup CI

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+    private String hello;
+
+    public WelcomeController(@Value("${welcome_message}") String hello) {
+        this.hello = hello;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return hello;
     }
 }
